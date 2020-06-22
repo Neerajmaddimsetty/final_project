b7526f31b56f5c4161032c889f5fec8115854844
M.Neeraj <srinulak3@gmail.com>
2020-06-22 09:24:34 +0530

    initial commut

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+var bodyParesr = require("body-parser");
+var mongoose = require("mongoose")
+var visitedp = require("./models/vmodel")
+var passport = require("passport")
+var LocalStrategy = require("passport-local")
+var User = require("./models/tuser");
+var methodOverride = require("method-override")
+var visitedRoutes = require("./routes/vroute")
+var indexRoutes = require("./routes/index")
+var flash = require("connect-flash")
+const { render } = require("ejs");
+mongoose.connect("mongodb+srv://yelp:yelp@cluster0-lfy4s.mongodb.net/yelp?retryWrites=true&w=majority", { useNewUrlParser: true })
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParesr.urlencoded({ extended: true }));
+app.set("view engine", "ejs")
+app.use(flash())
+app.use(express.static("public"));
+app.use(require("express-session")({
+    secret: "hi travelog",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+})
+app.use(methodOverride("_method"))
+app.use(visitedRoutes);
+app.use(indexRoutes);
+
+
+
+app.listen(3000, function() 
+{
+    console.log("Started")
+})
---
+++ js_files/route.js
@@ -0,0 +1,144 @@
+var express = require("express")
+var router = express.Router()
+var visitedp = require("../models/vmodel")
+var User = require("../models/vmodel")
+
+router.get("/", function(req, res) 
+{
+    res.render("Login")
+})
+
+router.get("/visited", isLoggedIn, function(req, res) 
+{
+    userid = req.user._id
+    query = { $and: [{ place: { $ne: "bucket" } }, { place: { $exists: true } }] }
+    final = { $and: [{ "user": userid }, query] }
+    visitedp.find({ "user": userid }, function(err, place) 
+    {
+        if (err) 
+          { 
+              console.log(err) 
+          } 
+          else 
+          {
+            visitedp.find(final, function(err, vplace) 
+            {
+                if (err) 
+                {
+                    console.log(err)
+                } 
+                else 
+                {
+                    res.render("vplace", { vplace: vplace })
+                }
+            })
+        }
+    })
+
+})
+
+router.post("/visited", isLoggedIn, function(req, res) 
+{
+    var name = req.body.name
+    var image = req.body.image
+    var about = req.body.about
+    var place = req.body.place
+    var date = req.body.date
+    var user = req.user._id
+    var newPlace = { name: name, image: image, about: about, place: place, date: date, user: user }
+    visitedp.create(newPlace, function(err, visited) {
+        if (err) 
+        {
+            console.log(err)
+        } 
+        else 
+        {
+            res.redirect("/visited")
+        }
+    })
+})
+router.get("/bucket", isLoggedIn, function(req, res) 
+{
+    userid = req.user._id
+    query = { $and: [{ place: { $ne: "visited" } }, { place: { $exists: true } }] }
+    final = { $and: [{ "user": userid }, query] }
+    visitedp.find({ "user": userid }, function(err, place) 
+    {
+        if (err) 
+        { 
+            console.log(err) 
+        } 
+        else 
+        {
+            visitedp.find(final, function(err, vplace) 
+            {
+                if (err) 
+                {
+                    console.log(err)
+                }
+                 else 
+                {
+                    res.render("bucket", { vplace: vplace })
+                }
+            })
+        }
+    })
+})
+
+router.get("/visited/new", isLoggedIn, function(req, res) 
+{
+    res.render("new.ejs")
+})
+
+router.get("/visited/:id/edit", function(req, res) 
+{
+    visitedp.findById(req.params.id, function(err, found) 
+    {
+        if (err) 
+        {
+            res.redirect("/visited")
+        } 
+        else 
+        {
+            res.render("edit", { found: found })
+        }
+    })
+
+})
+router.put("/visited/:id", function(req, res) 
+{
+    visitedp.findByIdAndUpdate(req.params.id, req.body.place, function(err, updated) 
+    {
+        if (err) 
+        {
+            res.redirect("/visited")
+        } 
+        else 
+        {
+            res.redirect("/visited")
+        }
+    })
+})
+
+router.delete("/visited/:id", function(req, res) {
+    visitedp.findByIdAndRemove(req.params.id, function(err) 
+    {
+        if (err) {
+            res.redirect("/visited")
+        } else {
+            res.redirect("/visited")
+        }
+    })
+})
+
+
+function isLoggedIn(req, res, next) 
+{
+    if (req.isAuthenticated()) 
+    {
+        return next();
+    }
+    res.redirect("/")
+}
+
+module.exports = router
